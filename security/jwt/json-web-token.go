76c52741b6bf49934e0ec42260b15fb0099e7e80
dmpol <dimasven@mail.ru>
2023-12-19 20:24:33 +0300

    v0.2

---
+++ security/jwt/json-web-token.go
@@ -0,0 +1,52 @@
+package jwt
+
+import (
+	"github.com/golang-jwt/jwt/v5"
+	"time"
+)
+
+const lifeTimeJWT int = 15
+
+var jwtSecretKey = []byte("my-super-secret-key-12345")
+
+type Claims struct {
+	Username string `json:"username"`
+	jwt.RegisteredClaims
+}
+
+func CreatingJWT(userName string) (string, error) {
+
+	tokenActionTime := time.Now().Add(time.Duration(lifeTimeJWT) * time.Minute)
+
+	claims := &Claims{
+		Username: userName,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(tokenActionTime),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	tokenString, err := token.SignedString(jwtSecretKey)
+
+	return tokenString, err
+}
+
+func DemarshJWT(tokenStr string) (string, *jwt.Token, error) {
+
+	claims := &Claims{}
+
+	tkn, err := jwt.ParseWithClaims(tokenStr, claims, func(token *jwt.Token) (interface{}, error) {
+		return jwtSecretKey, nil
+	})
+
+	return claims.Username, tkn, err
+}
+
+func TimeRefresh(claims *Claims) bool {
+	return time.Until(claims.ExpiresAt.Time) > time.Duration(lifeTimeJWT*60)*time.Second
+}
+
+func GetLifeTimeJWT() int {
+	return lifeTimeJWT
+}
---
+++ test.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	var t time.Duration = 5
+	fmt.Println(t * time.Minute)
+}
